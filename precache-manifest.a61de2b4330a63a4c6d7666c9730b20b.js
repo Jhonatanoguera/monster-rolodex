11b073a2c12af611d48ecff123861804df39c4d2
Jhonatan Noguera <jnoguera@pros.com>
2019-08-15 15:54:16 -0500

    Updates

---
+++ precache-manifest.a61de2b4330a63a4c6d7666c9730b20b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d648a5851a0533bbea3cf0de3697cf0c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3119a59f9a4620ca358d",
+    "url": "/monster-rolodex/static/css/main.f67f6d85.chunk.css"
+  },
+  {
+    "revision": "f7e71d2cca931e5dd749",
+    "url": "/monster-rolodex/static/js/2.89e16e46.chunk.js"
+  },
+  {
+    "revision": "3119a59f9a4620ca358d",
+    "url": "/monster-rolodex/static/js/main.487d0fa9.chunk.js"
+  },
+  {
+    "revision": "974ebdbc8482b2d59e77",
+    "url": "/monster-rolodex/static/js/runtime~main.cf73aa67.js"
+  }
+]);
